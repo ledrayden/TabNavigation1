fee7c8bab28ac44cfe71cb0befdd06102d0c2b48
terra <gamerdarkmatter@gmail.com>
2021-08-19 11:55:15 -0400

    firt commit

---
+++ app/components/BookInfo.js
@@ -0,0 +1,15 @@
+// image title author and review star
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const BookInfo = () => {
+    return (
+        <View style={styles.container}></View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {}
+})
+
+export default BookInfo;
